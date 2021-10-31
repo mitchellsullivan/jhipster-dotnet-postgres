c9efab687cc016ffc9dae1c3ac47ed0ee9094099
Mitchell Sullivan <ms@mitchellsullivan.net>
2021-10-31 12:03:50 -0500

    generalize migration script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 MIGRATION_NAME="$1";
-DB_CONTEXT="$2";
-CONF_ENV="${3:-$Development}";
+DB_CONTEXT="${2:-AppDbContext}";
+CONF_ENV="${3:-Development}";
 
-SLN='Plainly';
-MIGRATIONS_PROJECT="$SLN.Infrastructure";
+ROOT_NS='Plainly';
+MIGRATIONS_PROJECT="$ROOT_NS.Infrastructure";
 
 dotnet ef migrations add "$MIGRATION_NAME" \
-  --project "../src/$MIGRATIONS_PROJECT/$SLN.Infrastructure.csproj" \
-  --context "../src/$MIGRATIONS_PROJECT/$MIGRATIONS_PROJECT.Data.$DB_CONTEXT" \
-  --output-dir "../src/$MIGRATIONS_PROJECT/Data/Migrations" \
-  --startup-project "../src/$SLN/$SLN.csproj" \
+  --startup-project "../src/$ROOT_NS/$ROOT_NS.csproj" \
+  --project         "../src/$MIGRATIONS_PROJECT/$MIGRATIONS_PROJECT.csproj" \
+  --output-dir      'Data/Migrations' \
+  --context         "$MIGRATIONS_PROJECT.Data.$DB_CONTEXT" \
   --verbose \
   -- "$CONF_ENV";
 
 echo "Done adding migration $MIGRATION_NAME";
 
 dotnet ef database update \
-  --startup-project ../Plainly/Plainly.csproj \
-   -- "$CONF_ENV"
+  --startup-project "../src/$ROOT_NS/$ROOT_NS.csproj" \
+  -- "$CONF_ENV"
   
-echo "Done updating database with migration $MIGRATION_NAME"
+echo "Done updating database with migration $MIGRATION_NAME";
 
 
 
